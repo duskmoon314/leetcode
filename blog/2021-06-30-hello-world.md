064ac815719a1ec5e603b503a329241c74a97496
duskmoon <campbell.he@icloud.com>
2021-07-01 11:42:39 +0800

    feat: Setup docusaurus blog frame

---
+++ blog/2021-06-30-hello-world.md
@@ -0,0 +1,7 @@
+---
+slug: hello-world
+title: Hello
+tags: [hello]
+---
+
+尝试使用 [**Docusaurus 2**](https://docusaurus.io/) 的博客模式来记录我的 leetcode 解法。
---
+++ docusaurus.config.js
@@ -0,0 +1,60 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "暮月的 Leetcode 记录",
+  tagline: "Dinosaurs are cool",
+  url: "https://duskmoon314.github.io",
+  baseUrl: "/leetcode/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo.png",
+  organizationName: "duskmoon314",
+  projectName: "leetcode",
+  themeConfig: {
+    navbar: {
+      title: "暮月的 Leetcode 记录",
+      logo: {
+        alt: "暮月",
+        src: "img/logo.png",
+      },
+      items: [
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/duskmoon314/leetcode",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} duskmoon. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: false,
+        blog: {
+          blogTitle: "Duskmoon's leetcode solution",
+          editUrl:
+            "https://github.com/duskmoon314/leetcode/edit/master/website/blog/",
+          path: "./blog",
+          routeBasePath: "/",
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
